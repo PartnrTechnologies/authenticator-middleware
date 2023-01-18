ac29d668e2ebfd059f969961e4e1d864058ca0b4
Everton Marcelino Jr <51867881+EvertonMJunior@users.noreply.github.com>
2023-01-18 02:13:22 -0300

    chore: update build

--- build/index.js
+++ build/index.js
@@ -5,6 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.ensureScope = void 0;
 const axios_1 = __importDefault(require("axios"));
+const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
 require('dotenv').config();
 const AUTHENTICATOR_API_URL = process.env.AUTHENTICATOR_API_URL;
 const authenticateWithApiKey = async (req, res, next, apiKey) => {
@@ -23,6 +24,34 @@ const authenticateWithApiKey = async (req, res, next, apiKey) => {
         }
         else {
             return unavailable(res, "We weren't able to get details about your API key.");
+        }
+    })
+        .catch((error) => {
+        if (error.response && error.response.data) {
+            return res.json(error.response.data);
+        }
+        else {
+            return unavailable(res, "We weren't able to authenticate your request.");
+        }
+    });
+};
+const authenticateWithFirebase = async (req, res, next, bearerToken) => {
+    await axios_1.default
+        .post(AUTHENTICATOR_API_URL, {
+        bearer_token: bearerToken,
+        url: req.protocol + "://" + req.get("host") + req.originalUrl,
+        origin: req.headers["cf-connecting-ip"]
+    })
+        .then((response) => {
+        if (response.data && response.data.scopes) {
+            res.locals.scopes = response.data.scopes;
+            res.locals.user = response.data.user;
+            if (response.headers["request-id"])
+                res.set("Request-Id", response.headers["request-id"]);
+            return next();
+        }
+        else {
+            return unavailable(res, "We weren't able to get details about you.");
         }
     })
         .catch((error) => {
@@ -63,13 +92,23 @@ const forbidden = (res, message, missing_scope) => {
     });
 };
 const authenticator = async (req, res, next) => {
+    var _a;
     let { authorization } = req.headers;
     if (authorization !== undefined &&
         typeof authorization === 'string' &&
         authorization.startsWith('Bearer ') &&
         authorization.split('Bearer ').length === 2) {
-        const apiKey = authorization.split('Bearer ')[1];
-        return await authenticateWithApiKey(req, res, next, apiKey);
+        const bearerToken = authorization.split('Bearer ')[1];
+        const jwtData = (_a = jsonwebtoken_1.default.decode(bearerToken)) !== null && _a !== void 0 ? _a : {
+            iss: ""
+        };
+        let issuer = jwtData["iss"];
+        if (issuer !== undefined && issuer === "PARTNR LTDA") {
+            return await authenticateWithApiKey(req, res, next, bearerToken);
+        }
+        if (issuer !== undefined && issuer === "https://securetoken.google.com/partnr-technologies-production") {
+            return await authenticateWithFirebase(req, res, next, bearerToken);
+        }
     }
     unauthorized(res);
 };
